3c31679af8ccf597897a9782dce422b12b03bf04
John Doe <johndoe@zelax.ru>
2024-03-19 18:06:00 +0300

    add comments for offline

--- libs/offline.cpp
+++ libs/offline.cpp
@@ -1,6 +1,15 @@
 #include "offline.h"
+#include "function.h"
+
 
 void Offline::setPath(char* username){
     sprintf(this->path, "../offline/%s.txt", username);
 }
 
+bool Offline::checkFile(){
+    if(getFileSize(this->path) == 0){
+        return false;
+    } else {
+        return true;
+    }
+}
