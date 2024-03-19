dd471e7ecd2d4c23b5ab244840024bb865a7405a
John Doe <johndoe@zelax.ru>
2024-03-19 14:51:59 +0300

    +clearUserDB

--- libs/user.cpp
+++ libs/user.cpp
@@ -5,6 +5,7 @@ bool User::operator==(char* username){
 }
 
 void User::disconnect(){
+    shutdown(this->sock, SHUT_RDWR);
     close(this->sock);
     this->sock = -1;
 }
@@ -17,6 +18,25 @@ void Online::addUser(User& newUser){
     online.push_back(newUser);
 }
 
-User Online::findUser(char* username){
+User* Online::findUser(char* username){
+    for(auto i = online.begin(); i != online.end(); i++){
+        if(*i == username){
+            return &*i;
+        }
+    }
+    return NULL;
+}
 
+void Online::removeUser(char* username){
+    for(auto i = online.begin(); i != online.end(); i++){
+        i->disconnect();
+        online.erase(i);
+        return;
+    }
+}
+
+void Online::clearUserDB(){
+    for(auto i = online.begin(); i != online.end(); i++){
+        removeUser(i->username);       
+    }
 }
