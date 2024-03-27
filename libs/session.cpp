5994d78d329255229c355603b1952af9a21f6b67
John Doe <johndoe@zelax.ru>
2024-03-27 17:21:43 +0300

    +handleCommand +handleTimer

--- libs/session.cpp
+++ libs/session.cpp
@@ -9,20 +9,44 @@ Session::Session(int sock){
 int Session::waitFDs(){
     struct pollfd fds[2];
     nfds_t nfd = 2;
-    fds[0].fd = this->protocol.user->sock;
+    fds[0].fd = this->protocol.user->sock; // сокет-дескриптор пользователя
     fds[0].events = POLLIN | POLLRDHUP;
-    fds[1].fd = this->protocol.user->timer.getFirstTimer();
+
+    fds[1].fd = this->protocol.user->timer.getFirstTimer(); // дескриптор таймера
     fds[1].events = POLLIN | POLLRDHUP;
     int ret = 0;
     while(ret != -1){
         ret = poll(fds, 2, -1);
         if(ret > 0){
-            for(int i = 0; i < nfd; i++){
-                if(fds[i].revents != 0){
-                    
-                }
+            if(fds[0].revents != 0){
+                fds[0].revents = 0;
+                // TODO: сделать проверку, что ивентом является не HUP
+                this->handleCommand();
+            } else {
+                fds[1].revents = 0;
+                this->handleTimer();
             }
         }
     }
-    
+    this->end();
+}
+
+void Session::handleCommand(){
+    recvCommand(this->protocol.user->sock, this->protocol.user->bufferRecv);
+
+    if(this->protocol.user->bufferRecv->header.type == 0){
+        // отправка коамнды получателю по юзернейму
+    } else {
+        // отправка команды получателю по юзернейму
+        // удаление первого в очереди таймера
+        // удаление первого в очереди анконфирма
+    }
+}
+
+void Session::handleTimer(){
+    uint64_t exp;
+    int byte = read(this->protocol.user->timer.getFirstTimer(), &exp, sizeof(exp));
+    if(byte != -1){
+        this->end();
+    }
 }
